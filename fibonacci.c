c465f3edbb7a58c51686bb95f2ed57e628f3f9ed
Nicolas Henriquez <38541445+Nicolashex@users.noreply.github.com>
2021-12-10 22:20:10 -0300

    Add files via upload

--- fibonacci.c
+++ fibonacci.c
@@ -1,6 +1,6 @@
-#include <omp.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<omp.h>
+
 
 
 void primerfactors(long long num){
@@ -10,42 +10,55 @@ void primerfactors(long long num){
     for(count = 2; num>1; count++){
         while(num % count == 0){
             if(primero==0){
-                printf("%d ", count);
+                printf("= %d", count);
                 primero = 1;
             }
             else{
 
-                printf("x %d ", count);
+                printf(" x %d", count);
             }
 
             num = num/count;
         }
-    }
-}
-int fib(int n) {
-    long long i, j;
-    if (n < 2)
-        return n;
-    else {
-        #pragma omp taks shared(i) firstprivate(n)
-        i = fib(n - 1);
-        #pragma omp task shared(j) firstprivate(n)
-        j = fib(n - 2);
-        #pragma omp taskwait
-        return i + j;
-    }
-}
-int main() {
-    int n = 47;
-    long long result;
-    printf("result is ");
 
-    #pragma omp parallel shared(n)
+    }
+}
+
+void fibonacci(long long A[], long long N){
+    unsigned long long i;
+    A[0] = 0;
+    A[1] = 1;
+    for(i = 2; i < N; i++){
+        A[i] = A[i - 1] + A[i - 2];
+    }
+}
+
+
+int main(){
+    long long N = 47;
+    long long fib[N];
+    long long i;
+
+
+    
+    #pragma omp parallel sections
     {
-        #pragma omp single
-        result = fib(n);
+       
+        #pragma omp section
+        {
+            fibonacci(fib, N);
+        }
+    }
+
+
+    printf("Fibonacci Series:\n");
+    for(i = 0; i < N; i++){
+        printf("Fibonacci(%d): %d ", i+1, fib[i]);
+        primerfactors(fib[i]);
+        printf("\n");
 
     }
-    primerfactors(result);
+    printf("\n");
+
     return 0;
 }
